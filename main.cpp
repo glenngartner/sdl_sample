a5357e9eae0799fa368091a82ecda6115f648169
Glenn Gartner <glenngartner@gmail.com>
2017-11-24 22:53:03 -0500

    add SDL as CMAKE project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "SDL.h"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    SDL_Window *window = nullptr;
+    SDL_Surface *screenSurface = nullptr;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not be initialized! SDL Error: %s\n", SDL_GetError());
+    } else {
+        printf("SDL 2 initialized");
+    }
+
     return 0;
 }
